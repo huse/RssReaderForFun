541503c96ee42679502a6d5ec051e3d913612366
hus.kpr@gmail.com <hus.kpr@gmail.com>
2015-10-28 14:54:55 -0700

    click method ...

--- app/src/main/java/com/hosen/khp/rssreaderforfun/MainActivity.java
+++ app/src/main/java/com/hosen/khp/rssreaderforfun/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,7 +17,8 @@ import com.hosen.khp.rssreaderforfun.util.RssReader;
 
 import java.util.List;
 
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 
 
@@ -33,8 +35,9 @@ public class MainActivity extends Activity {
             ,button17,button21,button22,button23,button24,button25,button26,button27;
     GetRSSDataTask task;
     String urlString="";
+    String address;
     private String[] listData={"dddd","ffff", "hhhh"};
-
+    ImageView imagview = (ImageView) findViewById(R.id.icon);
     String yahoo= "http://news.yahoo.com/rss/";
     String google = "https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss";
     String bing = "https://www.bing.com/news/search?q=developer&go=Submit&qs=n&form=NWBQBN&pq=developer&sc=8-9&sp=-1&sk=&format=RSS";
@@ -73,8 +76,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         // Set view
         setContentView(R.layout.activity_main);
-
-
+        // This code is for add banner
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         // Set reference to this activity
         local = this;
@@ -91,6 +96,27 @@ public class MainActivity extends Activity {
         Log.d("RssReader", Thread.currentThread().getName());
 
         buttonGo = (Button)findViewById(R.id.button_go);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+        button5 = (Button)findViewById(R.id.button5);
+        button6 = (Button)findViewById(R.id.button6);
+        button7 = (Button)findViewById(R.id.button7);
+        button11 = (Button)findViewById(R.id.button11);
+        button12 = (Button)findViewById(R.id.button12);
+        button13 = (Button)findViewById(R.id.button13);
+        button14 = (Button)findViewById(R.id.button14);
+        button15 = (Button)findViewById(R.id.button15);
+        button16 = (Button)findViewById(R.id.button16);
+        button17 = (Button)findViewById(R.id.button17);
+        button21 = (Button)findViewById(R.id.button21);
+        button22 = (Button)findViewById(R.id.button22);
+        button23 = (Button)findViewById(R.id.button23);
+        button24 = (Button)findViewById(R.id.button24);
+        button25 = (Button)findViewById(R.id.button25);
+        button26 = (Button)findViewById(R.id.button26);
+        button27 = (Button)findViewById(R.id.button27);
         buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -103,64 +129,93 @@ public class MainActivity extends Activity {
             }
 
         });
-        button1 = (Button)findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                task = new GetRSSDataTask();
-
-                    Toast.makeText(getApplicationContext(), "http://news.yahoo.com/rss/", Toast.LENGTH_SHORT).show();
-                    task.execute(urlString);
-
-            }
-
-        });
-        button2 = (Button)findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                task = new GetRSSDataTask();
-                task.execute(urlString);
-                Log.d("RssReaderByButtonY", Thread.currentThread().getName());
 
 
-            }
+    }
+    public void onClick(View v) {
 
-        });
-        button3 = (Button)findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_go:
+                taskMethod(editText.getText().toString());
+
+                break;
+            case R.id.button1:
+                taskMethod(yahoo);
+                break;
+            case R.id.button2:
+                taskMethod(google);
+                break;
+            case R.id.button3:
+                taskMethod(bing);
+                break;
+            case R.id.button4:
+                taskMethod(cnn);
+                break;
+            case R.id.button5:
+                taskMethod(msn);
+                break;
+            case R.id.button6:
+                taskMethod(reuters);
+                break;
+            case R.id.button7:
+                taskMethod(msn);
+                break;
+            case R.id.button11:
+                taskMethod(mercedes);
+                break;
+            case R.id.button12:
+                taskMethod(porsche);
+                break;
+            case R.id.button13:
+                taskMethod(lamborghini);
+                break;
+            case R.id.button14:
+                taskMethod(lexus);
+                break;
+            case R.id.button15:
+                taskMethod(gm);
+                break;
+            case R.id.button16:
+                taskMethod(msn);
+                break;
+            case R.id.button17:
+                taskMethod(msn);
+                break;
+            case R.id.button21:
+                taskMethod(microsoft);
+                break;
+            case R.id.button22:
+                taskMethod(directv);
+                break;
+            case R.id.button23:
+                taskMethod(cisco);
+                break;
+            case R.id.button24:
+                taskMethod(att);
+                break;
+            case R.id.button25:
+                taskMethod(apple);
+                break;
+            case R.id.button26:
+                taskMethod(Sony);
+                break;
+            case R.id.button27:
+                taskMethod(msn);
+                break;
+
+        }
 
 
-                task = new GetRSSDataTask();
 
-                //Toast.makeText(getApplicationContext(), "Bing", Toast.LENGTH_SHORT).show();
-                task.execute(urlString);
+    }
 
-
-            }
-
-        });
-        button4 = (Button)findViewById(R.id.button2);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String urlString = "http://rss.cnn.com/rss/cnn_topstories.rss";
-
-                    task = new GetRSSDataTask();
-                    task.execute(urlString);
-                    Log.d("RssReaderBu", Thread.currentThread().getName());
-
-
-
-
-            }
-
-        });
-
+    private void taskMethod(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        urlString = s;
+        imagview.setImageBitmap( res/drawable/postthumb_loading);
+        task = new GetRSSDataTask();
+        task.execute(s);
+        Log.d("RssReaderBu", Thread.currentThread().getName());
     }
 
     private class GetRSSDataTask extends AsyncTask<String, Void, List<RssItem> > {
